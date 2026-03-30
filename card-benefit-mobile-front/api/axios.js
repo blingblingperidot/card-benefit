1504652c1a51440906196410df5e796d701b7098
blingblingperidot <blingblingperidot@gmail.com>
2026-03-30 13:17:50 +0900

    moblie package 설치 및 axios 파일변경

--- card-benefit-mobile-front/api/axios.js
+++ card-benefit-mobile-front/api/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const instance = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
@@ -7,5 +8,28 @@ const instance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+// 요청 인터셉터 - JWT 토큰 자동 첨부
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// 응답 인터셉터 - 401 시 로그인으로 이동
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      await AsyncStorage.removeItem('token');
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
