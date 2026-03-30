ac175db0aab2e7d272fe4be08e02a72a47965436
blingblingperidot <blingblingperidot@gmail.com>
2026-03-30 13:31:23 +0900

    fix: gitignore에 .metadata 추가

--- card-benefit-web-front/src/api/axios.js
+++ card-benefit-web-front/src/api/axios.js
@@ -8,4 +8,28 @@ const instance = axios.create({
   },
 });
 
+// 요청 인터셉터 - JWT 토큰 자동 첨부
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// 응답 인터셉터 - 401 시 로그인으로 이동
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default instance;
