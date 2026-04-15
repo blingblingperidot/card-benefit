e2f91f5bf679912ca87669a4bbcba6302a3b36e1
blingblingperidot <blingblingperidot@gmail.com>
2026-04-15 17:17:48 +0900

    payment test

--- card-benefit-payment-server/src/main/java/com/cardbenefit/payment/config/JwtUtil.java
+++ card-benefit-payment-server/src/main/java/com/cardbenefit/payment/config/JwtUtil.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Date;
-
+//payment jwt
 @Component
 public class JwtUtil {
 
