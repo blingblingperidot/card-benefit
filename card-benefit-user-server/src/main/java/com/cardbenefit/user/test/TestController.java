372f8b846b77e5048e99497599df4eb1b2ec5981
blingblingperidot <blingblingperidot@gmail.com>
2026-04-15 16:41:24 +0900

    userbuildtest중

--- card-benefit-user-server/src/main/java/com/cardbenefit/user/test/TestController.java
+++ card-benefit-user-server/src/main/java/com/cardbenefit/user/test/TestController.java
@@ -3,7 +3,7 @@ package com.cardbenefit.user.test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//테스트용 컨트롤러 
+//테스트
 @RestController
 @RequestMapping("/api/users")
 public class TestController {
